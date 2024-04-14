24b6e26377c0efbee20422153e2acb563c8124b4
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-14 11:35:00 +0400

    new commit!

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,12 @@
 export default Main;
 import "./main.css";
+import "./fix.css";
 import Sectionleft from "../Sectionleft/Sectionleft";
 import Sectionright from "../Sectionright/Sectionright";
 function Main() {
   return (
     <>
       <main>
-        just main
         <Sectionleft> </Sectionleft>
         <Sectionright></Sectionright>
       </main>
