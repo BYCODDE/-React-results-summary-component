2aecfd21d6edf598dcef19043a40fcffb7ddb96f
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-14 11:16:48 +0400

    new commit!

--- src/components/Sectionleft/Circle/Circle.jsx
+++ src/components/Sectionleft/Circle/Circle.jsx
@@ -1,9 +1,16 @@
-
 import "./circle.css";
 import circleSvg from "./circle.svg";
 
 function circle() {
-  return <img src={circleSvg} alt="circle" />;
+  return (
+    <>
+      <img src={circleSvg} alt="circle" />
+      <div className="score">
+        <span className="main_score">76</span>
+        <span className="under_score">of 100</span>
+      </div>
+    </>
+  );
 }
 
 export default circle;
